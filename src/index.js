9b74f208f6eec00369de3988197ac8a4d542f222
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2022-11-21 17:38:13 +0100

    fix: Fix konnector error having every data undefined

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ const {
   log
 } = require('cozy-konnector-libs')
 
-const moment = require('moment')
-
 const request = requestFactory({
   debug: false,
   cheerio: true,
@@ -44,7 +42,6 @@ async function start(fields) {
 
   log('info', 'Fetching the list of documents')
   let $ = await request(ordersListUrl)
-
   while ($) {
     log('info', 'Parsing list of documents')
     const pageDocuments = await parseDocuments($)
@@ -87,49 +84,42 @@ async function authenticate(username, password) {
 // The goal of this function is to parse a html page wrapped by a cheerio instance
 // and return an array of js objects which will be saved to the cozy by saveBills (https://github.com/konnectors/libs/blob/master/packages/cozy-konnector-libs/docs/api.md#savebills)
 async function parseDocuments($) {
-  const orders = scrape(
-    $,
-    {
-      date: {
-        sel: '.row:nth-child(1) > .column:nth-child(1) > .mythomann-order-teaser__detail > .mythomann-order-teaser__detail-value',
-        parse: parseDate
-      },
-      number: {
-        sel: '.row:nth-child(1) > .column:nth-child(2) > .mythomann-order-teaser__detail > .mythomann-order-teaser__detail-value',
-        parse: parseOrderNumber
-      },
-      amount: {
-        sel: '.row:nth-child(2) > .column:nth-child(1) > .mythomann-order-teaser__detail > .mythomann-order-teaser__detail-value',
-        parse: parseAmount
-      },
-      currency: {
-        sel: '.row:nth-child(2) > .column:nth-child(1) > .mythomann-order-teaser__detail > .mythomann-order-teaser__detail-value',
-        parse: parseCurrency
-      },
-      details: {
-        sel: '.mythomann-order-teaser__button-row a',
-        attr: 'href'
-      }
-    },
-    '#order-list > .mythomann-order-teaser > .mythomann-order-teaser__details-wrapper > .mythomann-order-teaser__details'
-  )
-
+  const orders = []
+  $('.mythomann-order-teaser').each((i, el) => {
+    const details = $(el)
+      .find('.mythomann-order-teaser__details-wrapper')
+      .html()
+      .match(/a href="(.*)>/g)[0]
+      .split('"')[1]
+    const billsData = $(el)
+      .find('.mythomann-order-teaser__detail')
+      .text()
+      .replace(/\s\s+/g, ' ')
+    const number = billsData.match(/Commande ([0-9.]*) Prix/g)[0].split(' ')[1]
+    const amount = billsData
+      .match(/total (.*)€/)[1]
+      .trim()
+      .replace(',', '.')
+    const currency = '€'
+    const billDate = billsData.match(/du ([0-9.]*)/g)[0].split(' ')[1]
+    const [day, month, year] = billDate.split('.')
+    const date = `${year}-${month}-${day}`
+    orders.push({ amount, currency, date, details, number })
+  })
   let documents = []
   for (let order of orders) {
     const $details = await request(order.details)
     const fileurl = $details(
       'a.mythomann-order-history__entry-pdf-button'
     ).attr('href')
-    const filename = `${order.date.format(
-      'YYYY-MM-DD'
-    )}_${vendor}_${order.amount.toFixed(2)}${order.currency}_${
-      order.number
-    }.pdf`
+    const filename = `${order.date}_${vendor}_${parseFloat(
+      order.amount
+    ).toFixed(2)}${order.currency}_${order.number}.pdf`
 
     documents.push({
       vendor: vendor,
-      date: order.date.toDate(),
-      amount: order.amount,
+      date: new Date(order.date),
+      amount: parseFloat(order.amount),
       currency: order.currency,
       fileurl: fileurl,
       filename: filename,
@@ -166,27 +156,4 @@ async function findNextDocumentsPage($) {
   }
 
   return null
-}
-
-function parseOrderNumber(number) {
-  return number.slice(number.lastIndexOf(' ') + 1).trim()
-}
-
-function parseAmount(price) {
-  const amountStr = price
-    .trim()
-    .slice(price.indexOf(':') + 1, price.length)
-    .slice(0, price.length - 1)
-    .replace(',', '.')
-
-  return parseFloat(amountStr)
-}
-
-function parseCurrency(price) {
-  return price.trim()[price.length - 1]
-}
-
-function parseDate(date) {
-  const dateStr = date.slice(date.lastIndexOf(' ') + 1, date.length).trim()
-  return moment.utc(dateStr, 'DD.MM.YYYY')
 }
